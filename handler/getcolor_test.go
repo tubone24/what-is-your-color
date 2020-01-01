1480c6f54356561538bbed9b6591d5a36067b65b
Yu Otsubo <uu-ootsubo@kddi.com>
2020-01-01 18:38:33 +0900

    wip

--- handler/getcolor_test.go
+++ handler/getcolor_test.go
@@ -2,13 +2,14 @@ package handler
 
 import (
 	"github.com/stretchr/testify/assert"
+	//"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"github.com/labstack/echo/v4"
 )
 
-const userJSON = `{"name":"Jon Snow","email":"jon@labstack.com"}`
+const userJSON = ``
 
 func TestGetColor(t *testing.T) {
 	// Setup
@@ -19,6 +20,8 @@ func TestGetColor(t *testing.T) {
 	c.SetPath("/get/:username")
 	c.SetParamNames("username")
 	c.SetParamValues("tubone24")
+	//h := GetColor()
+	//require.NoError(t, h(c))
 	assert.Equal(t, http.StatusOK, rec.Code)
 	assert.Equal(t, userJSON, rec.Body.String())
 }
