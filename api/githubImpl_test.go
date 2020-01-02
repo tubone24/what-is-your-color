59a5f5095ce646663521a1c5571f26826fd5e63d
Yu Otsubo <uu-ootsubo@kddi.com>
2020-01-02 16:02:03 +0900

    fix testcase name because of that you should not use lowercase in function name

--- api/githubImpl_test.go
+++ api/githubImpl_test.go
@@ -9,11 +9,20 @@ import (
 type TestClientImpl struct {
 }
 
-func TestlangContainsTrue(t *testing.T) {
+func TestLangContainsTrue(t *testing.T) {
 	var input []models.GitHubLang
 	input = append(input, models.GitHubLang{Name: "Python", Color: "test", Size: 1})
 	input = append(input, models.GitHubLang{Name: "JavaScript", Color: "test2", Size: 2})
 	actualBool, actualIndex := langsContains(input, "Python")
 	assert.Equal(t, true, actualBool)
 	assert.Equal(t, 0, actualIndex)
+}
+
+func TestLangContainsFalse(t *testing.T) {
+	var input []models.GitHubLang
+	input = append(input, models.GitHubLang{Name: "Python", Color: "test", Size: 1})
+	input = append(input, models.GitHubLang{Name: "JavaScript", Color: "test2", Size: 2})
+	actualBool, actualIndex := langsContains(input, "Go")
+	assert.Equal(t, false, actualBool)
+	assert.Equal(t, -1, actualIndex)
 }
